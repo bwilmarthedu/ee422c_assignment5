18d65c019f6e68cdf2111b51b671afdaebbd2c90
kassandrasmith <kassandrasmith@utexas.edu>
2018-04-02 22:31:50 -0500

    Add InputValues Class

---
+++ src/assignment5/InputValues.java
@@ -0,0 +1,11 @@
+package assignment5;
+
+public class InputValues {
+    public Critter makeWhichCritter;
+    public int amtOfCritters;
+    public int animateTimeStep;
+    public int runTimeStep; //todo look back over requirements and see if this is how that should work
+    public int seedNumber;
+
+
+}
